cc628aed0cfe1bbbff2f69b31fc9b5385c0a2537
manrique <administrador@Ubuntu22.04>
2024-04-19 10:09:58 +0200

    hola

--- calculadora.java
+++ calculadora.java
@@ -1,3 +1,1 @@
-Una calculadora es un dispositivo que se utiliza para realizar cálculos aritméticos.
- Aunque las calculadoras modernas se incorporan a menudo en un ordenador de propósito general,
- se diseñan para realizar ciertas operaciones para que sean más flexibles.
+hola
